7b7bc195c0ef94c0fa6972c54970e8afb310b918
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2021-09-20 20:58:08 +0200

    Added classes and interface

---
+++ Inlämningsupg 3 - zork/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämningsupg_3___zork
+{
+    class Character
+    {
+        public string Name { get; set; }
+        public List<Item> ItemList { get; set; }
+        public string LastDirection { get; set; }
+        public int Score { get; set; }
+        public int Moves { get; set; }
+    }
+}
---
+++ Inlämningsupg 3 - zork/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämningsupg_3___zork
+{
+    class Location
+    {
+        public bool IsEndPoint { get; set; }
+        public List<Exit> ExitList { get; set; }
+        public List<Item> ItemList { get; set; }
+    }
+}
