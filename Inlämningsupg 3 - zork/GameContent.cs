81036b4a1b9e2b502eb67d637e9594b5063b8ed6
Tobias Acebo <tobie07@hotmail.com>
2021-10-03 18:04:44 +0200

    Did the scenario 2

--- Inlämningsupg 3 - zork/GameContent.cs
+++ Inlämningsupg 3 - zork/GameContent.cs
@@ -20,7 +20,7 @@ namespace Inlämningsupg_3___zork
                     Description = "Years of traveling in the Mediterranean sea. \r\nYou have finally arrived in Kattegatt with your ship.  \r\nHere at the docks, you must find your way into the great halls. \r\n\r\nAt the end of the docks, there's a fisherman talking about his travels with his men. \r\nPerhaps he knows how to get into the great halls. ",
                     LocationList = new List<Location>
                     {
-                        new Location {Title = "starting point", Description = "Ah here we are, back were we started. At the end of the docks I can see a fisherman talking about his travels with his men. "}, 
+                        new Location {Title = "starting point", Description = "Ah here we are, back were we started. At the end of the docks I can see a fisherman talking about his travels with his men. "},
                         new Location {Title = "water", Description = "Oh no! You fell into the water. Try and climb back up the docks."},
                         new Location {Title = "boat", Description = "You have boarded the ship."},
                         new Location {Title = "end of docks", ItemList = new List<Item>{new Item {Title = "knife"}, new Item{Title = "fishing line" } }},
@@ -38,13 +38,18 @@ namespace Inlämningsupg_3___zork
                     Description = "You have entered Muddy road. \r\n A mist settels over the abandoned houses. \r\n The sign of life seems to have left this place. \r\n if you are lucky, perhaps someone is still around.",
                     LocationList = new List<Location>
                     {
-                        new Location {Title = "starting point", Description = "We are inside the gate from the docks"},
-                        new Location {Title = "forward", Description = "We are middle of the road"},
-                        new Location {Title = "back", Description = "You can't go back this way!"},
-                        new Location {Title = "west", Description = "Oh look it is a house with stacks of logs..."},
-                        new Location {Title = "east", Description = "Oh it is a house with a shield on th wall!"},
-                    }
-                }
+                        new Location {Title = "starting point", Description = "We are inside the gate from the docks. \r\n We can see a house stack of logs \r\n and a house with a shield on the wall..."},
+                        new Location {Title = "middle of the road", Description = "We are middle of the road."},
+                        new Location {Title = "gate the docks", Description = "You can't go back this way!"},
+                        new Location {Title = "house with stack full of logs", Description = "Oh look it is a house with stacks of logs..."},
+                        new Location {Title = "house with a sheild on the wall", ItemList = new List<Item>{ new Item { Title = "spear"}, new Item {Title = "helmet"}, new Item {Title = "book"}, new Item {Title = "candle" }}},
+                        new Location {Title = "house with carpet made of fur", Description = "It is a house with a carpet made of fur."},
+                        new Location {Title = "house next to a water well", Description = "It might be something in the well."},
+                        new Location {Title = "town gate", Description = "The gate leads us to the town"}
+                    },
+
+                    IsEndPoint = false
+                },
             
             };
         }
